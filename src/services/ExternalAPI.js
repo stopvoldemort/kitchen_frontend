196fdde81b0f4b5f3c894dcda2f01d0ec7fa3afa
stopvoldemort <david.squires1@gmail.com>
2019-11-18 10:18:53 -0500

    hi

--- src/services/ExternalAPI.js
+++ src/services/ExternalAPI.js
@@ -2,7 +2,7 @@ export default class ExternalAPI {
 
   static geocoder(address) {
     const baseUrl = "https://maps.googleapis.com/maps/api/geocode/json"
-    const apiParam = "AIzaSyAZ15pKC2voMB4vV9Hm3hj910Hf76BUAtM"
+    const apiParam = "let's not post this here anymore"
     const addressParam = address.replace(/\s+/g, '+')
     const url = baseUrl + "?address=" + addressParam + "&key=" + apiParam
     return fetch(url)
